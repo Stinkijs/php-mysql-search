4dc86c87f3821f52c6ec2070dba434c439c1f758
Stinkijs <stinkijs.minecraftien@gmail.com>
2021-10-21 16:25:51 +0200

    Mon nouveau projet

---
+++ database.php
@@ -0,0 +1,3 @@
+<?php
+$bdd = new PDO('mysql:host=LOCALHOST;dbname=DBNAME', 'ROOT', 'PASSWORD'); 
+?>
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+require_once 'database.php';
+$allusers = $bdd->query('SELECT * FROM users ORDER BY id DESC');
+if(isset($_GET['pseudo']) AND !empty($_GET['pseudo'])) {
+    $recherche = htmlspecialchars($_GET['pseudo']);
+    $allusers = $bdd->query('SELECT pseudo FROM users WHERE pseudo LIKE "%'.$recherche.'%" ORDER BY id DESC');
+}
+?>
+<html>
+  <head>
+    <title>Index</title>
+  </head>
+  <body>
+      <form action="" method="GET" class="form-example">
+        <div class="form-example">
+          <input type="search" name="pseudo" id="pseudo">
+        </div>
+        <div class="form-example">
+          <input type="submit" name="envoi" value="Rechercher">
+      </div>
+    </form>
+    <?php
+        if($allusers->rowCount() > 0) {
+            while($user = $allusers->fetch()) {
+                ?>
+                <p><?= $user['pseudo']; ?></p>
+                <?php
+            }
+        } else {
+              ?>
+              <p>Aucun utilisateur trouvé</p>
+              <?php
+        }
+      ?>
+  </body>
+</html>
